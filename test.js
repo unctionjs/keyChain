4ca95cf8353a6e6c86d9a3835c62ef7e5250fd2c
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-30 13:31:49 -0700

    Adding tests for maps

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-/* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type, no-undefined */
+/* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type, no-undefined, no-magic-numbers */
 import {test} from "tap"
 
 import keyChain from "./index"
 
-test("With realized keychain", ({same, end}) => {
+test(({same, end}) => {
   same(
     keyChain(["aaa", "bbb", "ccc"])({aaa: {bbb: {ccc: "1"}}}),
     "1"
@@ -12,9 +12,46 @@ test("With realized keychain", ({same, end}) => {
   end()
 })
 
-test("With missing key", ({same, end}) => {
+test(({same, end}) => {
   same(
     keyChain(["aaa", "ddd", "ccc"])({aaa: {bbb: {ccc: "1"}}}),
+    undefined
+  )
+
+  end()
+})
+
+test(({same, end}) => {
+  same(
+    keyChain(["aaa", "bbb", "ccc"])(new Map([["aaa", new Map([["bbb", new Map([["ccc", "ccc"]])]])]])),
+    "ccc"
+  )
+
+  end()
+})
+
+test(({same, end}) => {
+  same(
+    keyChain(["aaa", "ddd", "ccc"])(new Map([["aaa", new Map([["bbb", new Map([["ccc", "ccc"]])]])]])),
+    undefined
+  )
+
+  end()
+})
+
+
+test(({same, end}) => {
+  same(
+    keyChain([0, 0, 0])([[["1"]]]),
+    "1"
+  )
+
+  end()
+})
+
+test(({same, end}) => {
+  same(
+    keyChain([0, 1, 0])([[["1"]]]),
     undefined
   )
 
